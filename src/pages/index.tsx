7a765620fd4f1881843c901fd5e212578bbc1160
Sejmou <samo.kolter@gmail.com>
2023-03-16 14:02:56 +0100

    Learn Dancing -> Learn to Dance

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,7 +44,7 @@ const Login = () => {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 text-white">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Learn <span className="text-[hsl(280,100%,70%)]">Dancing</span>
+          Learn to <span className="text-[hsl(280,100%,70%)]">Dance</span>
         </h1>
         <div className="flex flex-col items-center gap-2">
           <Button variant="transparent" onClick={() => void signIn()}>
